693b50fc1e0194d6a894635e766707b5e23ed452
rockybindra <rocky.bindra@protonmail.com>
2025-11-13 00:31:38 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -29,7 +29,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'alabaster'
-html_title = "to Install, Download, and Reinstall Quicken on a New Computer: Step-by-Step Guide via Quicken.com"
+html_title = "to Redeem, Download, and Install VIPRE Already Purchased: A Quick Start Guide for Existing Key Holders"
 
 # You can still add html_meta here if you want, but with the new Read the Docs addons
 # custom template is the reliable way to inject meta tags.
